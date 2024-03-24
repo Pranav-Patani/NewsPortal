911fcd4cf4c9f9bbd45992be794d1784a3bba451
Pranav-Patani <pranav.patani@gmail.com>
2024-03-25 02:01:57 +0530

    fix: fixed scroll, now always nvaigate to top of the page

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,19 @@ import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { FeedProvider } from "./contexts/FeedContext.jsx";
+import ScrollToTop from "./components/ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <FeedProvider>
         <Auth0Provider
-            domain="dev-wwk5lsdpd6c8g6p6.us.auth0.com"
-            clientId="6UnRqDQk8RZIFSZpbCpWOV9AANYSTgxa"
-            authorizationParams={{
-              redirect_uri: window.location.origin
-            }}
+          domain="dev-wwk5lsdpd6c8g6p6.us.auth0.com"
+          clientId="6UnRqDQk8RZIFSZpbCpWOV9AANYSTgxa"
+          authorizationParams={{
+            redirect_uri: window.location.origin,
+          }}
         >
           <App />
         </Auth0Provider>
